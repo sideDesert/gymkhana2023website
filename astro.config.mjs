b1bd8f93be3dd13e1c71e703e5b154b74b58602e
Siddarth Saha <siddarthsaha1@gmail.com>
2022-12-24 14:56:32 +0530

    added vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 import image from "@astrojs/image";
-import vercel from "@astrojs/vercel";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
