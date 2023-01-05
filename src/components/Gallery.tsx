f229e27c9a0e1c21ee2eac06cc334978dc1b4c4f
Siddarth Saha <siddarthsaha1@gmail.com>
2023-01-05 09:54:53 +0530

    added new gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,13 +1,14 @@
 import { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination, Navigation, Autoplay } from "swiper";
+import { EffectCoverflow, Pagination, Navigation, Autoplay } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/navigation";
 import "swiper/css/autoplay";
+import "swiper/css/effect-coverflow";
 
 import "./Gallery.css";
 
@@ -26,12 +27,30 @@ export default function Gallery() {
         navigation={true}
         loop={true}
         autoplay={true}
-        pagination={{
-          clickable: true,
+        effect={"coverflow"}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={1}
+        breakpoints={{
+          "@1.00": {
+            slidesPerView: 2,
+            spaceBetween: 40,
+          },
+          "@1.5": {
+            slidesPerView: 3,
+            spaceBetween: 0,
+          },
+        }}
+        coverflowEffect={{
+          rotate: 0,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
         }}
         autoHeight={true}
-        modules={[Pagination, Navigation, Autoplay]}
-        className="mySwiper text-black w-full h-auto md:max-w-3xl"
+        modules={[Pagination, Navigation, Autoplay, EffectCoverflow]}
+        className="mySwiper text-black w-full h-auto "
       >
         {images.map((src, index) => {
           return (
