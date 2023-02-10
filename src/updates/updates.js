c88ffdb179e8788d895152f811814873f368e321
Siddarth Saha <siddarthsaha1@gmail.com>
2023-02-10 18:01:52 +0530

    tried to fix url errors for mobile

--- src/updates/updates.js
+++ src/updates/updates.js
@@ -1,57 +1,49 @@
 const updates = [
   {
-    title: 'INDIAN HIP HOP DANCE CHAMPIONSHIP 2023 Auditions Season 12',
-    desc: 'Hyderabad !!! IHHDC is coming to your city.. are you ready to feel the thrill on stage? .\
+    title: "INDIAN HIP HOP DANCE CHAMPIONSHIP 2023 Auditions Season 12",
+    desc: "Hyderabad !!! IHHDC is coming to your city.. are you ready to feel the thrill on stage? .\
         Gear up yourself to have a blasting experience in Hyderabad Regional Auditions of\
-        INDIAN HIP HOP DANCE CHAMPIONSHIP 2023, SEASON 12! is here.. in  IIT Hyderabad in collaboration with Elan & ηVision',
-    img: '/images/updates section/1.jpg',
-    organiser: 'Elan & ηVision',
+        INDIAN HIP HOP DANCE CHAMPIONSHIP 2023, SEASON 12! is here.. in  IIT Hyderabad in collaboration with Elan & ηVision",
+    img: "/images/updates section/1.jpg",
+    organiser: "Elan & ηVision",
   },
   {
-    title: 'Cryptex, Elan & ηVision 2023',
+    title: "Cryptex, Elan & ηVision 2023",
     desc: "Cryptex, the Online Treasure Hunt organized as part of Elan & ηVision, the techno-cultural fest of IIT Hyderabad is back with its 7th edition. \
 \
     Cryptex is a 54-hour long engaging hunt that comprises a series of questions containing cryptic images, videos, and audio clips among other such codes and ciphers.\
     As you breeze through the levels and move ahead with some brain-picking and clever Googling, we're sure you'll have one of the most mind-boggling and engrossing experiences of your life. Explore a whole new universe of ciphers and puzzles through Cryptex, which will keep you on the edge of your seat. \
     \
     Mark your calendars, for Cryptex will go live on February 10th, 6:00 PM IST.",
-    img: '/images/updates section/3.jpg',
-    dateTime: 'February 20th, 2023 (6:00 PM IST)',
-    organiser: 'Elan & ηVision',
+    img: "/images/updates section/3.jpg",
+    dateTime: "February 20th, 2023 (6:00 PM IST)",
+    organiser: "Elan & ηVision",
   },
   {
-    title: 'Elan & ηVision: EML Talk',
+    title: "Elan & ηVision: EML Talk",
     desc: "We present to you our first EML Talk for the fest on the topic, 'Creative thinking inside-the-box' by Sesha Kanthamraju.\
     He is currently the CEO of World's largest film studio - Ramoji Film City",
-    img: '/images/updates section/eml.jpeg',
-    speaker: 'Sesha Kanthamraju, CEO of Ramoji Film City',
-    dateTime: 'February 18th, 2023 (11:30 AM IST)',
-    organiser: 'Elan & ηVision',
+    img: "/images/updates section/eml.jpeg",
+    speaker: "Sesha Kanthamraju, CEO of Ramoji Film City",
+    dateTime: "February 18th, 2023 (11:30 AM IST)",
+    organiser: "Elan & ηVision",
   },
   {
-    title: 'Jigyasa: A quiz on Indian heritage, culture and Sanskrit',
-    desc: 'Every Indian Culture, Heritage and Samskrit enthusiast is most welcome to be a part of this event. The Quiz will test your knowledge and understanding of Indian Culture, Heritage, Samskrit in traditions, and Samskrit Bhasha. Students are requested to participate in the Quiz in groups of 3 to 5. This time we are coming up with an exciting prize pool of Rs.3500. You are encouraged to name your team as per the quiz theme.',
-    img: '/images/updates section/sanskrit.jpeg',
-    dateTime: 'February 10th, 2023 (8:30 PM IST)',
-    organiser: 'Samskritam - IITH',
-    venue: 'A LH 2',
+    title: "Jigyasa: A quiz on Indian heritage, culture and Sanskrit",
+    desc: "Every Indian Culture, Heritage and Samskrit enthusiast is most welcome to be a part of this event. The Quiz will test your knowledge and understanding of Indian Culture, Heritage, Samskrit in traditions, and Samskrit Bhasha. Students are requested to participate in the Quiz in groups of 3 to 5. This time we are coming up with an exciting prize pool of Rs.3500. You are encouraged to name your team as per the quiz theme.",
+    img: "/images/updates section/sanskrit.jpeg",
+    dateTime: "February 10th, 2023 (8:30 PM IST)",
+    organiser: "Samskritam - IITH",
+    venue: "A LH 2",
   },
   {
-    title: 'The 65 to 90 of Pytorch: Introduction to Pytorch',
-    desc: "We are excited to announce 'The 65 to 90 of Pytorch'- a session for all ML enthusiasts and budding aspirants. PyTorch is an open-source machine learning library that is used in numerous fields such as Computer Vision, Natural Language Processing, Deep Learning, etc. This session is designed to help you learn the basics of PyTorch and show you how to use it for your projects.",
-    img: '/images/updates section/acm.jpeg',
-    dateTime: 'February 9th, 2023 (9:00 PM IST)',
-    organiser: 'ACM - IITH',
-    venue: 'A LH 1',
-  },
-  {
-    title: 'Irshaad',
-    desc: 'Want to perform in IIT Hyderabad in front of a huge audience ???\
+    title: "Irshaad",
+    desc: "Want to perform in IIT Hyderabad in front of a huge audience ???\
     Then we got it covered for you! Recite some of your mesmerizing pieces and also win some exciting prizes worth 14,000! \
-    Register quickly and confirm your slot,as the link closes soon!projects.',
-    img: '/images/updates section/irshaad.jpeg',
-    dateTime: 'February 18th, 2023',
-    organiser: 'Elan & ηVision - IITH',
+    Register quickly and confirm your slot,as the link closes soon!projects.",
+    img: "/images/updates section/irshaad.jpeg",
+    dateTime: "February 18th, 2023",
+    organiser: "Elan & ηVision - IITH",
     //venue: 'A LH 1',
   },
 ];
