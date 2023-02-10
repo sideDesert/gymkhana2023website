c4dd96402c5aafb36aebede5aed620694d70f56c
gaurang-dahad <105926845+gaurang-dahad@users.noreply.github.com>
2023-02-11 01:28:35 +0530

    added tinkerers lab in clubs, zozimus in events, fixed navbar and events dropdown

--- src/components/events page/Options.tsx
+++ src/components/events page/Options.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { useState, useEffect, useRef } from "react";
+import React from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 function Options({ eventId }) {
   const [event, setEvent] = useState(eventId);
@@ -11,8 +11,8 @@ function Options({ eventId }) {
   useEffect(() => {
     let u = new URL(window.location.href);
     setUrl(u);
-    let new_url: string | string[] = u.toString().split("/");
-    if (new_url[new_url.length - 1] === "") {
+    let new_url: string | string[] = u.toString().split('/');
+    if (new_url[new_url.length - 1] === '') {
       new_url.pop();
     }
     let la = new_url.pop();
@@ -22,16 +22,16 @@ function Options({ eventId }) {
     optionsRef.current.value = la;
   }, []);
   useEffect(() => {
-    console.log(window.location.href.split("/").pop());
+    console.log(window.location.href.split('/').pop());
     if (url) {
       if (event !== last_address) {
-        let new_url: string | string[] = url.toString().split("/");
-        if (new_url[new_url.length - 1] === "") {
+        let new_url: string | string[] = url.toString().split('/');
+        if (new_url[new_url.length - 1] === '') {
           new_url.pop();
         }
         new_url.pop();
         new_url.push(event);
-        new_url = new_url.join("/");
+        new_url = new_url.join('/');
 
         window.location.href = new_url;
       }
@@ -52,12 +52,13 @@ function Options({ eventId }) {
         }}
         name="events"
         id=""
-        className="event-selection px-5 py-3 bg-transparent border-2 rounded-lg text-center flex justify-center"
+        className="event-selection px-5 py-3 bg-black border-2 rounded-lg text-center flex justify-center"
       >
         <option value="elan">Elan & nVision</option>
         <option value="e-summit">E-Summit</option>
         <option value="milan">Milan</option>
         <option value="ted-talks">Ted-Talks</option>
+        <option value="zozimus">Zozimus</option>
         <option value="abhyant">Abhyant</option>
         <option value="diesta">Diesta</option>
         <option value="almuni-day">Almuni Day</option>
