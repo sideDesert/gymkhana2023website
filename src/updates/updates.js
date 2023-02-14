52978d139f6823abd8d9fcbb49b89899d84af878
gaurang-dahad <105926845+gaurang-dahad@users.noreply.github.com>
2023-02-14 09:26:50 +0530

    added sports secy 2022 pic, removed cryptex from updates

--- src/updates/updates.js
+++ src/updates/updates.js
@@ -1,70 +1,58 @@
 const updates = [
   {
-    title: "INDIAN HIP HOP DANCE CHAMPIONSHIP 2023 Auditions Season 12",
-    desc: "Hyderabad !!! IHHDC is coming to your city.. are you ready to feel the thrill on stage? .\
+    title: 'INDIAN HIP HOP DANCE CHAMPIONSHIP 2023 Auditions Season 12',
+    desc: 'Hyderabad !!! IHHDC is coming to your city.. are you ready to feel the thrill on stage? .\
         Gear up yourself to have a blasting experience in Hyderabad Regional Auditions of\
-        INDIAN HIP HOP DANCE CHAMPIONSHIP 2023, SEASON 12! is here.. in  IIT Hyderabad in collaboration with Elan & ηVision",
-    img: "/images/updates section/1.jpg",
-    organiser: "Elan & ηVision",
+        INDIAN HIP HOP DANCE CHAMPIONSHIP 2023, SEASON 12! is here.. in  IIT Hyderabad in collaboration with Elan & ηVision',
+    img: '/images/updates section/1.jpg',
+    organiser: 'Elan & ηVision',
   },
   {
-    title: "Cryptex, Elan & ηVision 2023",
-    desc: "Cryptex, the Online Treasure Hunt organized as part of Elan & ηVision, the techno-cultural fest of IIT Hyderabad is back with its 7th edition. \
-\
-    Cryptex is a 54-hour long engaging hunt that comprises a series of questions containing cryptic images, videos, and audio clips among other such codes and ciphers.\
-    As you breeze through the levels and move ahead with some brain-picking and clever Googling, we're sure you'll have one of the most mind-boggling and engrossing experiences of your life. Explore a whole new universe of ciphers and puzzles through Cryptex, which will keep you on the edge of your seat. \
-    \
-    Mark your calendars, for Cryptex will go live on February 10th, 6:00 PM IST.",
-    img: "/images/updates section/3.jpg",
-    dateTime: "February 20th, 2023 (6:00 PM IST)",
-    organiser: "Elan & ηVision",
-  },
-  {
-    title: "Elan & ηVision: EML Talk",
+    title: 'Elan & ηVision: EML Talk',
     desc: "We present to you our first EML Talk for the fest on the topic, 'Creative thinking inside-the-box' by Sesha Kanthamraju.\
     He is currently the CEO of World's largest film studio - Ramoji Film City",
-    img: "/images/updates section/eml.jpeg",
-    speaker: "Sesha Kanthamraju, CEO of Ramoji Film City",
-    dateTime: "February 18th, 2023 (11:30 AM IST)",
-    organiser: "Elan & ηVision",
+    img: '/images/updates section/eml.jpeg',
+    speaker: 'Sesha Kanthamraju, CEO of Ramoji Film City',
+    dateTime: 'February 18th, 2023 (11:30 AM IST)',
+    organiser: 'Elan & ηVision',
   },
   {
-    title: "Blackout Poetry",
-    desc: "The Writing Club is hosting a unique event that promises to be both entertaining and inspiring. On Monday, 13th February, the club invites all literary enthusiasts to join them for a Blackout Poetry and Open Mic Session. For those unfamiliar with the concept, Blackout Poetry involves taking a page from an existing text, such as a newspaper or a novel, and blacking out unrequired words to create a new poem. The process encourages creativity and is a new way of looking at the written word. \
-    There is also an opportunity for attendees to perform their own writing with the Open Mic Session. Whether you are a seasoned poet or just want to showcase your musings, this event promises to be an inclusive and encouraging space for all.",
-    img: "/images/updates section/blackout-poetry.jpg",
-    dateTime: "February 13th, 2023 (9:00 PM IST)",
-    organiser: "The Writing Club",
-    venue: "A LH-1",
+    title: 'Blackout Poetry',
+    desc: 'The Writing Club is hosting a unique event that promises to be both entertaining and inspiring. On Monday, 13th February, the club invites all literary enthusiasts to join them for a Blackout Poetry and Open Mic Session. For those unfamiliar with the concept, Blackout Poetry involves taking a page from an existing text, such as a newspaper or a novel, and blacking out unrequired words to create a new poem. The process encourages creativity and is a new way of looking at the written word. \
+    There is also an opportunity for attendees to perform their own writing with the Open Mic Session. Whether you are a seasoned poet or just want to showcase your musings, this event promises to be an inclusive and encouraging space for all.',
+    img: '/images/updates section/blackout-poetry.jpg',
+    dateTime: 'February 13th, 2023 (9:00 PM IST)',
+    organiser: 'The Writing Club',
+    venue: 'A LH-1',
   },
   {
-    title: "Irshaad",
-    desc: "Want to perform in IIT Hyderabad in front of a huge audience ???\
+    title: 'Irshaad',
+    desc: 'Want to perform in IIT Hyderabad in front of a huge audience ???\
     Then we got it covered for you! Recite some of your mesmerizing pieces and also win some exciting prizes worth 14,000! \
-    Register quickly and confirm your slot,as the link closes soon!projects.",
-    img: "/images/updates section/irshaad.jpeg",
-    dateTime: "February 18th, 2023",
-    organiser: "Elan & ηVision - IITH",
+    Register quickly and confirm your slot,as the link closes soon!projects.',
+    img: '/images/updates section/irshaad.jpeg',
+    dateTime: 'February 18th, 2023',
+    organiser: 'Elan & ηVision - IITH',
     //venue: 'A LH 1',
   },
   {
-    title: "Culti Events: Elan & ηVision",
-    desc: "Presenting to you all the culti events ..!!!\
+    title: 'Culti Events: Elan & ηVision',
+    desc: 'Presenting to you all the culti events ..!!!\
     Participants in the competition will have access to pronities.\
-    For registering for the events: https://elan.org.in/competitions",
-    img: "/images/updates section/culti.jpeg",
-    dateTime: "February 17th, 18th and 19th, 2023",
-    organiser: "Elan & ηVision - IITH",
+    For registering for the events: https://elan.org.in/competitions',
+    img: '/images/updates section/culti.jpeg',
+    dateTime: 'February 17th, 18th and 19th, 2023',
+    organiser: 'Elan & ηVision - IITH',
     //venue: 'A LH 1',
   },
   {
-    title: "Techy Events: Elan & ηVision",
-    desc: "We present before you the ηVision part of our fest, Techy competition are there!\
+    title: 'Techy Events: Elan & ηVision',
+    desc: 'We present before you the ηVision part of our fest, Techy competition are there!\
 Get ready to participate in these amazing events and win exciting prizes.\
-Mark your calendars!",
-    img: "/images/updates section/techy.jpeg",
-    dateTime: "February 17th, 18th and 19th, 2023",
-    organiser: "Elan & ηVision - IITH",
+Mark your calendars!',
+    img: '/images/updates section/techy.jpeg',
+    dateTime: 'February 17th, 18th and 19th, 2023',
+    organiser: 'Elan & ηVision - IITH',
     //venue: 'A LH 1',
   },
 ];
