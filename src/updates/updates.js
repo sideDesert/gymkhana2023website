46c0072ecaba0eb8bea6f1fddbb31ac7b8aa8dcd
gaurang-dahad <105926845+gaurang-dahad@users.noreply.github.com>
2023-02-21 22:26:02 +0530

    removed events from updates and added new ones

--- src/updates/updates.js
+++ src/updates/updates.js
@@ -1,59 +1,21 @@
 const updates = [
+  // {
+  //   title: 'Elan & ηVision: EML Talk',
+  //   desc: "We present to you our first EML Talk for the fest on the topic, 'Creative thinking inside-the-box' by Sesha Kanthamraju.\
+  //   He is currently the CEO of World's largest film studio - Ramoji Film City",
+  //   img: '/images/updates section/eml.jpeg',
+  //   speaker: 'Sesha Kanthamraju, CEO of Ramoji Film City',
+  //   dateTime: 'February 18th, 2023 (11:30 AM IST)',
+  //   organiser: 'Elan & ηVision',
+  // },
   {
-    title: 'INDIAN HIP HOP DANCE CHAMPIONSHIP 2023 Auditions Season 12',
-    desc: 'Hyderabad !!! IHHDC is coming to your city.. are you ready to feel the thrill on stage? .\
-        Gear up yourself to have a blasting experience in Hyderabad Regional Auditions of\
-        INDIAN HIP HOP DANCE CHAMPIONSHIP 2023, SEASON 12! is here.. in  IIT Hyderabad in collaboration with Elan & ηVision',
-    img: '/images/updates section/1.jpg',
-    organiser: 'Elan & ηVision',
-  },
-  {
-    title: 'Elan & ηVision: EML Talk',
-    desc: "We present to you our first EML Talk for the fest on the topic, 'Creative thinking inside-the-box' by Sesha Kanthamraju.\
-    He is currently the CEO of World's largest film studio - Ramoji Film City",
-    img: '/images/updates section/eml.jpeg',
-    speaker: 'Sesha Kanthamraju, CEO of Ramoji Film City',
-    dateTime: 'February 18th, 2023 (11:30 AM IST)',
-    organiser: 'Elan & ηVision',
-  },
-  {
-    title: 'Blackout Poetry',
-    desc: 'The Writing Club is hosting a unique event that promises to be both entertaining and inspiring. On Monday, 13th February, the club invites all literary enthusiasts to join them for a Blackout Poetry and Open Mic Session. For those unfamiliar with the concept, Blackout Poetry involves taking a page from an existing text, such as a newspaper or a novel, and blacking out unrequired words to create a new poem. The process encourages creativity and is a new way of looking at the written word. \
-    There is also an opportunity for attendees to perform their own writing with the Open Mic Session. Whether you are a seasoned poet or just want to showcase your musings, this event promises to be an inclusive and encouraging space for all.',
-    img: '/images/updates section/blackout-poetry.jpg',
-    dateTime: 'February 13th, 2023 (9:00 PM IST)',
-    organiser: 'The Writing Club',
-    venue: 'A LH-1',
-  },
-  {
-    title: 'Irshaad',
-    desc: 'Want to perform in IIT Hyderabad in front of a huge audience ???\
-    Then we got it covered for you! Recite some of your mesmerizing pieces and also win some exciting prizes worth 14,000! \
-    Register quickly and confirm your slot,as the link closes soon!projects.',
-    img: '/images/updates section/irshaad.jpeg',
-    dateTime: 'February 18th, 2023',
-    organiser: 'Elan & ηVision - IITH',
-    //venue: 'A LH 1',
-  },
-  {
-    title: 'Culti Events: Elan & ηVision',
-    desc: 'Presenting to you all the culti events ..!!!\
-    Participants in the competition will have access to pronities.\
-    For registering for the events: https://elan.org.in/competitions',
-    img: '/images/updates section/culti.jpeg',
-    dateTime: 'February 17th, 18th and 19th, 2023',
-    organiser: 'Elan & ηVision - IITH',
-    //venue: 'A LH 1',
-  },
-  {
-    title: 'Techy Events: Elan & ηVision',
-    desc: 'We present before you the ηVision part of our fest, Techy competition are there!\
-Get ready to participate in these amazing events and win exciting prizes.\
-Mark your calendars!',
-    img: '/images/updates section/techy.jpeg',
-    dateTime: 'February 17th, 18th and 19th, 2023',
-    organiser: 'Elan & ηVision - IITH',
-    //venue: 'A LH 1',
+    title: 'Bohemian Rhapsody',
+    desc: 'Qurve is delighted to invite you to our next event, in collaboration with BMC, FRIDAY MOVIE NIGHT! \
+    We will be screening Bohemian Rhapsody (2018), a biographical music about Freddy Mercury, the lead singer of the rock band Queen. Join us with friends, snacks, blankets and enjoy the movie together!',
+    img: '/images/updates section/bohemian.jpeg',
+    dateTime: 'February 24th, 2023 (9:00 pM IST)',
+    organiser: 'Qurve and BMC',
+    venue: 'B113 and B115',
   },
 ];
 
