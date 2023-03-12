65dff12c99d02b4c5fd067d9b5d62e2565dc0f6e
gaurang-dahad <105926845+gaurang-dahad@users.noreply.github.com>
2023-03-12 13:12:22 +0530

    added zozimus to updates

--- src/updates/updates.js
+++ src/updates/updates.js
@@ -1,14 +1,5 @@
 const updates = [
   // {
-  //   title: 'Elan & ηVision: EML Talk',
-  //   desc: "We present to you our first EML Talk for the fest on the topic, 'Creative thinking inside-the-box' by Sesha Kanthamraju.\
-  //   He is currently the CEO of World's largest film studio - Ramoji Film City",
-  //   img: '/images/updates section/eml.jpeg',
-  //   speaker: 'Sesha Kanthamraju, CEO of Ramoji Film City',
-  //   dateTime: 'February 18th, 2023 (11:30 AM IST)',
-  //   organiser: 'Elan & ηVision',
-  // },
-  // {
   //   title: 'Bohemian Rhapsody',
   //   desc: 'Qurve is delighted to invite you to our next event, in collaboration with BMC, FRIDAY MOVIE NIGHT! \
   //   We will be screening Bohemian Rhapsody (2018), a biographical music about Freddy Mercury, the lead singer of the rock band Queen. Join us with friends, snacks, blankets and enjoy the movie together!',
@@ -17,6 +8,23 @@ const updates = [
   //   organiser: 'Qurve and BMC',
   //   venue: 'B113 and B115',
   // },
+  {
+    title: 'Zozimus 2023',
+    desc: `Return of the ZoZ!\n
+
+      'There are a whole lot of things in this world of ours you haven't started wondering about yet.'-Roald Dahl \n
+      Welcome to a bizarre universe filled with classic literature but with a few twists, where nothing is as it seems, and reality is only a fabrication of your mind.\n
+      
+      From the works of Kafka to Arundathi Roy, from Mordor to Lilliput, from the Wizard of (Z)oz to Don Quixote, you can experience all the literature you grew up with and discover more.\n
+      
+      Take a break from your daily life and visit us to get enthralled by the ingenious ways the classics can be reinvented.\n
+      
+      Presenting to you the 3rd Edition of IITH's annual literary fest, Zozimus: Through the Looking glass.`,
+    img: '/images/updates section/zoz.jpg',
+    dateTime: 'April 8th and 9th, 2023',
+    organiser: 'Litsoc',
+    //venue: 'B113 and B115',
+  },
 ];
 
 export default updates;
